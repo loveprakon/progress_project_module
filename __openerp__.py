1264d0744780cb998f12a743778e23b269c3aad7
loveprakon <loveprakon@gmail.com>
2019-10-19 23:21:39 +0700

    [APV] set module

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'progress_project_enet',
+    'version': '1.0.0',
+    'category': 'field',
+    'description': """
+        this is progress project field for enet department
+        """,
+    'author': 'prakon wiriyatanavirod',
+    'website': 'www.kmutnb.ac.th',
+    'depends': [],
+    'demo': [],
+    'data': [],
+    'update_xml': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
